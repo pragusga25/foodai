df7aebd7417360984e8968980a2039efa9f78b7a
Taufik Pragusga <taufik@pragusga.com>
2024-03-08 06:59:24 +0700

    feat(seo): update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,8 +38,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: 'https://foodai.vercel.app/logo.png',
-        width: 520,
-        height: 520,
+        width: 1200,
+        height: 630,
         alt: 'Food AI',
       },
     ],
@@ -55,8 +55,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: 'https://foodai.vercel.app/logo.png',
-        width: 520,
-        height: 520,
+        width: 1200,
+        height: 630,
         alt: 'Food AI',
       },
     ],
